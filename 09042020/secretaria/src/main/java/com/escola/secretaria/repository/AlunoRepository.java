4d68c8d9fb975e1a05f304d6948bae0c80ff1db7
Daniel Cipriano <61021523+dancipri@users.noreply.github.com>
2020-09-15 19:23:41 -0300

    Add files via upload

---
+++ 09042020/secretaria/src/main/java/com/escola/secretaria/repository/AlunoRepository.java
@@ -0,0 +1,13 @@
+package com.escola.secretaria.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.escola.secretaria.model.Aluno;
+
+public interface AlunoRepository extends JpaRepository <Aluno, Long>{
+	
+	public List<Aluno> findAllByNomeContainingIgnoreCase(String nome);
+
+}
